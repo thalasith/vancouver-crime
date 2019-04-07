2d4915a10f32db2ee67939ced4200a055a12cad4
Tsao <boris-tsao@mercer.com>
2019-04-06 23:32:36 -0700

    second commit!

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    df = pd.read_csv('./data/vancouverCrime2018.csv')
+    chart_data = df.to_dict(orient='records')
+    chart_data = json.dumps(chart_data, indent=2)
+    data = {'chart_data': chart_data}
+
+    return render_template("index.html", data=data)
+
+
+@app.route('/api/<variable>', methods=['GET'])
+def data(variable):
+    link = './data/vancouverCrime' + variable + '.csv'
+    df = pd.read_csv(link)
+    chart_data = df.to_dict(orient='records')
+    chart_data = json.dumps(chart_data, indent=2)
+    data = {'chart_data': chart_data}
+
+    return data
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- config/staging.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  'app_host_port': 'click-that-hood-staging.herokuapp.com'
-}
